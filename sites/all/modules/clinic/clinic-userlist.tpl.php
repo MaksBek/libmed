b11727ebde1e8faef0bb9e375d0f2d21a4842a39
Maxim Bek <m.bek@bvblogic.com>
2014-01-22 22:50:44 +0200

    asdasd

--- sites/all/modules/clinic/clinic-userlist.tpl.php
+++ sites/all/modules/clinic/clinic-userlist.tpl.php
@@ -1,3 +1,15 @@
 <?php
-var_dump($data);
-echo 'bla bla bla';
+ //var_dump($users);
+ echo '<a href="/personal/add">[Добавить сотрудника]</a>';
+if(count($users)){
+    echo '<ul>'; 
+    foreach($users as $user){
+        if(isset($user->image) && $user->image != NULL){
+            $image = '<img src="' . file_create_url($user->image) . '" width="100" />';
+        } else {
+            $image = '<img src="/sites/default/files/pictures/no-image-blog.png" width="100" />';
+        }
+        echo '<li>' . $image . ' ' . $user->name . '<span style="float:right"><a href="/personal/' . $user->uid . '/edit">[Редактировать]</a></span></li>';
+    }
+    echo '</ul>';
+}
